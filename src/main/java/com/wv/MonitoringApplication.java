5377e7597c23fd49c612fb9a892bb3a628f84e21
fullth <lth129@naver.com>
2022-01-05 08:50:21 +0900

    화면소스 수정 및 build.gradle 수정 - layout fragment 태그 수정(head-> div) - jsGrid div 위치 변경

--- src/main/java/com/wv/MonitoringApplication.java
+++ src/main/java/com/wv/MonitoringApplication.java
@@ -2,9 +2,17 @@ package com.wv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MonitoringApplication {
+public class MonitoringApplication extends SpringBootServletInitializer {
+
+    /** 외장톰캣 사용을 위한 오버라이딩 */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MonitoringApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MonitoringApplication.class, args);
