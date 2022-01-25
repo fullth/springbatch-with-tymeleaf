a322fb0bb59ebc4469853e20c3b2be47f5083930
fullth <lth129@naver.com>
2022-01-25 14:12:18 +0900

    메인 페이지 레이아웃 수정

--- src/main/java/com/wv/monitoring/controller/HomeController.java
+++ src/main/java/com/wv/monitoring/controller/HomeController.java
@@ -26,11 +26,21 @@ public class HomeController {
 
         modelAndView.setViewName("index");
 
+        Date time = new Date();
+        SimpleDateFormat DATE_FORMAT = new SimpleDateFormat ( "yyyyMMdd");
+        String formattedTime = DATE_FORMAT.format(time);
+
         int batchCount = batchStatusService.selectBatchCount();
         int successCount = batchStatusService.selectCompleteStatusCount();
         int failCount = batchStatusService.selectFailStatusCount();
 
+        int todaySuccessCount = batchStatusService.selectTodayCompleteStatusCount(formattedTime);
+        int todayFailCount = batchStatusService.selectTodayFailStatusCount(formattedTime);
+
+        int todayBatchCount = todaySuccessCount + todayFailCount;
+
         modelAndView.addObject("batchCount", batchCount);
+        modelAndView.addObject("todayBatchCount", todayBatchCount);
         modelAndView.addObject("successCount", successCount);
         modelAndView.addObject("failCount", failCount);
 
