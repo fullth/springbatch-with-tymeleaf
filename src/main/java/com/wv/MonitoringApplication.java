4e8b036950361516d57bada9159ba9fc0ae13648
fullth <lth129@naver.com>
2022-02-15 10:26:47 +0900

    배포 설정 변경

--- src/main/java/com/wv/MonitoringApplication.java
+++ src/main/java/com/wv/MonitoringApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class MonitoringApplication extends SpringBootServletInitializer {
 
-    /** 외장톰캣 사용을 위한 오버라이딩 */
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(MonitoringApplication.class);
